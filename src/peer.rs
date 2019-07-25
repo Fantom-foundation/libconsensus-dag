7ad36c0cccf79c9d69ef3874c14b02e63a06625e
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-07-25 16:02:30 +1000

    src/peer.rs: add peer list struct

--- src/peer.rs
+++ src/peer.rs
@@ -34,3 +34,46 @@ pub struct DAG0Peer {
     #[serde(skip, default)]
     last_finalised_frame: Frame,
 }
+
+pub(crate) struct PeerList {
+    peers: Vec<BaseConsensusPeer>,
+    // number of peers; the size of the peer list
+    n: usize,
+    // round robin number
+    r: usize,
+}
+
+impl Default for PeerList {
+    fn default() -> PeerList {
+        PeerList {
+            peers: Vec::with_capacity(5),
+            n: 0,
+            r: 0,
+        }
+    }
+}
+
+impl PeerList {
+    fn add(&mut self, p: BaseConsensusPeer) -> bool {
+        if self.peers.len() == std::usize::MAX {
+            return false;
+        }
+        self.peers.push(p);
+        self.n = self.peers.len();
+        self.r = self.n >> 1;
+        true
+    }
+
+    fn next_peer(&mut self) -> BaseConsensusPeer {
+        // we assume the very first peer in the vector is one
+        // cotrresponding to the current node, so the value of `current`
+        // is always 0 and omitted here.
+        let next = 1 + self.r % (self.n - 1);
+        if self.r > 0 {
+            self.r >>= 1;
+        } else {
+            self.r = self.n >> 1
+        }
+        return self.peers[next].clone();
+    }
+}
